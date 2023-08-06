1a14ab94c22f761eca631da12fad9b1131d892ff
qianer-fengtian <perplex0112358@gmail.com>
2023-08-06 11:47:23 +0900

    Modified.

--- alias.fish
+++ alias.fish
@@ -1,2 +1,5 @@
-alias l "exa -li --icons"
+alias l "exa -gli --icons"
 alias bat "batcat"
+
+alias ymd "date +'%Y-%m-%d'"
+alias unixtime "date +%s"
--- git.fish
+++ git.fish
@@ -1,4 +1,8 @@
+git config --global core.editor vim
+
+# alias
 git config --global alias.s status
 git config --global alias.c commit
 git config --global alias.graph "log --oneline --decorate --graph --branches --tags --remotes --all"
 git config --global alias.g graph
+git config --global alias.d diff
